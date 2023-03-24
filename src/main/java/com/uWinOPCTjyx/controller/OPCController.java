491426194df694570c5cc7984958710382b102ad
lilekang <2955730886@qq.com>
2023-03-24 15:14:05 +0800

    fee

--- src/main/java/com/uWinOPCTjyx/controller/OPCController.java
+++ src/main/java/com/uWinOPCTjyx/controller/OPCController.java
@@ -6,10 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uWinOPCTjyx.entity.*;
 import com.uWinOPCTjyx.service.*;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/opc")
@@ -28,12 +30,19 @@ public class OPCController {
 	}
 
 	@RequestMapping(value="/opcm")
-	public String goOpcM(HttpServletRequest request) {
+	public String goOpcM(HttpServletRequest request, Model model) {
 
 		//localhost:8080/UWinOPCTjyx/opc/test
 		List<ZhiLiangZhongJianBzz> list = zhiLiangZhongJianBzzService.getList();
 		System.out.println("size==="+list.size());
-
 		return MODULE_NAME+"/opcm";
+	}
+
+	@RequestMapping("/getUser")
+	@ResponseBody
+	public User getUser(){
+		System.out.println("进来");
+		User user=new User(1,"张三");
+		return user;
 	}
 }
--- src/main/java/com/uWinOPCTjyx/entity/ZhiLiangZhongJianBzz.java
+++ src/main/java/com/uWinOPCTjyx/entity/ZhiLiangZhongJianBzz.java
@@ -5,22 +5,77 @@ public class ZhiLiangZhongJianBzz {
 	private Integer id;
 	private Integer zlx;
 	private Integer scjllx;
+	private Double bzzsx;
+	private Double bzzxx;
+	private String dw;
+	private Integer lx;
+
+	public ZhiLiangZhongJianBzz() {
+	}
+
+	public ZhiLiangZhongJianBzz(Integer id, Integer zlx, Integer scjllx, Double bzzsx, Double bzzxx, String dw, Integer lx) {
+		this.id = id;
+		this.zlx = zlx;
+		this.scjllx = scjllx;
+		this.bzzsx = bzzsx;
+		this.bzzxx = bzzxx;
+		this.dw = dw;
+		this.lx = lx;
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getZlx() {
 		return zlx;
 	}
+
 	public void setZlx(Integer zlx) {
 		this.zlx = zlx;
 	}
+
 	public Integer getScjllx() {
 		return scjllx;
 	}
+
 	public void setScjllx(Integer scjllx) {
 		this.scjllx = scjllx;
+	}
+
+	public Double getBzzsx() {
+		return bzzsx;
+	}
+
+	public void setBzzsx(Double bzzsx) {
+		this.bzzsx = bzzsx;
+	}
+
+	public Double getBzzxx() {
+		return bzzxx;
+	}
+
+	public void setBzzxx(Double bzzxx) {
+		this.bzzxx = bzzxx;
+	}
+
+	public String getDw() {
+		return dw;
+	}
+
+	public void setDw(String dw) {
+		this.dw = dw;
+	}
+
+	public Integer getLx() {
+		return lx;
+	}
+
+	public void setLx(Integer lx) {
+		this.lx = lx;
 	}
 }
