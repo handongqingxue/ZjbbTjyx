6b94a02c6e407bd4c36c88383aeb8150ea21e23e
lilekang <2955730886@qq.com>
2023-04-14 17:48:35 +0800

    11

--- src/main/java/com/uWinOPCTjyx/entity/OpcVarTest.java
+++ src/main/java/com/uWinOPCTjyx/entity/OpcVarTest.java
@@ -5,13 +5,7 @@ public class OpcVarTest {
     private Integer Id;
     private String VarName;
     private Double VarValue;
-    private String Unit;
-    private String RecType;
-    private Integer DealBz;
-    private String UpdateTime;
-    private Integer ParaType;
-    private Integer FId;
-    private String Desc;
+
 
     public Integer getId() {
         return Id;
@@ -37,61 +31,7 @@ public class OpcVarTest {
         VarValue = varValue;
     }
 
-    public String getUnit() {
-        return Unit;
-    }
 
-    public void setUnit(String unit) {
-        Unit = unit;
-    }
-
-    public String getRecType() {
-        return RecType;
-    }
-
-    public void setRecType(String recType) {
-        RecType = recType;
-    }
-
-    public Integer getDealBz() {
-        return DealBz;
-    }
-
-    public void setDealBz(Integer dealBz) {
-        DealBz = dealBz;
-    }
-
-    public String getUpdateTime() {
-        return UpdateTime;
-    }
-
-    public void setUpdateTime(String updateTime) {
-        UpdateTime = updateTime;
-    }
-
-    public Integer getParaType() {
-        return ParaType;
-    }
-
-    public void setParaType(Integer paraType) {
-        ParaType = paraType;
-    }
-
-    public Integer getFId() {
-        return FId;
-    }
-
-    public void setFId(Integer FId) {
-        this.FId = FId;
-    }
-
-    public String getDesc() {
-        return Desc;
-    }
-
-    public void setDesc(String desc) {
-        Desc = desc;
-    }
 
 
 
