a455811104ba9e6a6b27427cad1469459da81d60
handongqingxue <1417140290@qq.com>
2023-05-05 14:49:10 +0800

    1

--- src/main/java/com/zjbbTjyx/dao/TriggerVarMapper.java
+++ src/main/java/com/zjbbTjyx/dao/TriggerVarMapper.java
@@ -1,6 +1,7 @@
 package com.zjbbTjyx.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 
@@ -12,7 +13,13 @@ public interface TriggerVarMapper {
 
 	int add(TriggerVar triggerVar);
 
+	int addByList(@Param("triggerVarList") List<TriggerVar> triggerVarList);
+
 	int editByVarName(TriggerVar triggerVar);
 	
 	List<TriggerVar> getListByFIdList(@Param("runFIdList") List<Integer> runFIdList);
+
+	List<TriggerVar> getListByVarNameList(@Param("varNameList") List<String> varNameList);
+
+	int editByList(@Param("triggerVarList") List<TriggerVar> triggerVarList);
 }
--- src/main/java/com/zjbbTjyx/service/serviceImpl/TriggerVarServiceImpl.java
+++ src/main/java/com/zjbbTjyx/service/serviceImpl/TriggerVarServiceImpl.java
@@ -1,6 +1,10 @@
 package com.zjbbTjyx.service.serviceImpl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,66 @@ public class TriggerVarServiceImpl implements TriggerVarService {
 	public int editFromList(List<TriggerVar> triggerVarList) {
 		// TODO Auto-generated method stub
 		int editCount=0;
+		List<TriggerVar> addTVList=new ArrayList<>();
+		List<TriggerVar> editTVList=new ArrayList<>();
+		TriggerVar tv=null;
+		
+		List<String> varNameList=new ArrayList<>();
+		for (TriggerVar triggerVar : triggerVarList) {
+			varNameList.add(triggerVar.getVarName());
+		}
+		
+		//https://blog.csdn.net/weixin_45137708/article/details/120866268
+		//https://blog.csdn.net/weixin_38423249/article/details/80503491
+		List<TriggerVar> existTVList=triggerVarMapper.getListByVarNameList(varNameList);
+		for (TriggerVar triggerVar : triggerVarList) {
+			String varName = triggerVar.getVarName();
+			Float varValue = triggerVar.getVarValue();
+			
+			tv=new TriggerVar();
+			tv.setVarName(varName);
+			tv.setVarValue(varValue);
+			
+			boolean exist=false;
+			for (TriggerVar existTV : existTVList) {
+				if(varName.equals(existTV.getVarName())) {
+					exist=true;
+					break;
+				}
+			}
+			
+			if(exist) {
+				editTVList.add(tv);
+			}
+			else {
+				boolean containF = StringUtils.contains(varName, "F");
+				boolean containU = StringUtils.contains(varName, TriggerVar.U);
+				if (containF){//判断变量名称里面包含F
+					String recType=null;
+					if (containU){//包含U就是U类
+						recType=TriggerVar.U;
+					}
+					else {//不是U类就是M类
+						recType=TriggerVar.M;
+					}
+					tv.setRecType(recType);
+					
+					int fWz = varName.lastIndexOf("F");//F的位置
+					char f = varName.charAt(fWz + 1);//获得反应釜号
+					int fyfh=Integer.parseInt(String.valueOf(f));
+					tv.setFId(fyfh);
+				}
+				addTVList.add(tv);
+			}
+		}
+
+		if(addTVList.size()>0)
+			editCount+=triggerVarMapper.addByList(addTVList);
+		if(editTVList.size()>0)
+			editCount+=triggerVarMapper.editByList(editTVList);
+		System.out.println("editCount==="+editCount);
+		
+		/*
 		for (TriggerVar triggerVar : triggerVarList) {
 			String varName = triggerVar.getVarName();
 			int count=triggerVarMapper.getCountByVarName(varName);
@@ -41,6 +105,9 @@ public class TriggerVarServiceImpl implements TriggerVarService {
 			else
 				editCount+=triggerVarMapper.editByVarName(triggerVar);
 		}
+		*/
+		
+		
 		return editCount;
 	}
 
